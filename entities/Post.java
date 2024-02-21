af11889365bb9a36b2aa1290dec384794398db66
enricogth <enrigazoth@gmail.com>
2024-02-21 11:23:01 -0300

    first commit

---
+++ entities/Post.java
@@ -0,0 +1,99 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Post {
+
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
+	private Date moment;
+	private String title;
+	private String content;
+	private Integer likes;
+	
+	private List<Comment> comments = new ArrayList<>();
+	
+	//create a contructor empty
+	public Post() {
+		
+	}
+
+	//create the contructor with all, less the comments, because its a List
+	public Post(Date moment, String title, String content, Integer likes) {
+		this.moment = moment;
+		this.title = title;
+		this.content = content;
+		this.likes = likes;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getLikes() {
+		return likes;
+	}
+
+	public void setLikes(Integer likes) {
+		this.likes = likes;
+	}
+
+	public List<Comment> getComments() {
+		return comments;
+	}
+
+	//use comments.add to improve more comments, and erase the setComments
+	public void addComment(Comment comment) {
+		comments.add(comment);
+	}
+	
+	//to remove one comment, use this
+	public void romoveComment(Comment comment) {
+		comments.remove(comment);
+	}
+	
+	
+	//use StringBuilder to show all these Strings
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(title + "\n");
+		sb.append(likes);
+		sb.append(" Likes - ");
+		sb.append(sdf.format(moment) + "\n");
+		sb.append(content + "\n");
+		sb.append("Comments:\n");
+		for (Comment c : comments) {
+			sb.append(c.getText() + "\n");
+		}
+		
+		
+		return sb.toString();
+	}
+	
+	
+	
+	
+}
